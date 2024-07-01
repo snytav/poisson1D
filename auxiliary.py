5ff3796726edc22e907f3ca4f1ca2b38ae1de6c0
Alexey Snytnikov <snytav@gmail.com>
2024-07-02 00:22:36 +0200

    boundary conditions not working

--- auxiliary.py
+++ auxiliary.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 def A(x):
-    return (0.5*x[0])
+    return (0.5*x[0] if x[0] == 0.0 or x[0] == 1.0 else 0.0)
 
 def psy_trial(x, net_out):
     return A(x) + x[0] * (1 - x[0])  * net_out
